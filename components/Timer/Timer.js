af7d790d080efd724311314693dd8fc35745649c
M96RU <manuelrusso@laposte.net>
2024-08-13 22:17:12 +0200

    Add countdown and buttons

--- components/Timer/Timer.js
+++ components/Timer/Timer.js
@@ -1,34 +1,88 @@
 import React, {useEffect} from 'react';
-import {Button, StyleSheet, Text, Vibration, View,} from 'react-native';
+import {Alert, Button, StyleSheet, Text, Vibration, View,} from 'react-native';
 
 const Timer = () => {
-    const [time, setTime] = React.useState(0);
+
+    const afterBreak = 90;
+    const secondToPLay = 45;
+
+    const now = () => {
+        return new Date().getTime();
+    }
+
+    // current time - updated asynchronously
+    const [time, setTime] = React.useState(now());
+
+    // last click time
+    const [clicked, setClicked] = React.useState(0);
+
+    // last click time
+    const [timeToPlay, setTimeToPlay] = React.useState(afterBreak);
+
+    // true after break
+    const [alreadyBreak, setAlreadyBreak] = React.useState(false);
+
+    // true after alert
+    const [alreadyAlert, setAlreadyAlert] = React.useState(false);
+    // true after alert
+    const [alreadyAlertEnd, setAlreadyAlertEnd] = React.useState(false);
 
     useEffect(() => {
         const id = setTimeout(() => {
-            if (time > 0) {
-                setTime((prev) =>
-                    prev - 1
-                );
-            }
-        }, 1000);
+            setTime(now())
+        }, 100);
         return () => {
             clearTimeout(id);
         };
     });
 
     const start = () => {
-        setTime(90);
+        setClicked(now());
+        if (alreadyBreak) {
+            setAlreadyBreak(true);
+        }
+    }
+    const restart = () => {
+        setClicked(0);
+        setTimeToPlay(afterBreak);
+        setAlreadyBreak(false);
+        setAlreadyAlert(false);
+        setAlreadyAlertEnd(false);
+    }
+    const next = () => {
+        setAlreadyBreak(true);
+        setTimeToPlay(secondToPLay);
+        setClicked(now());
+        setAlreadyAlert(false);
+        setAlreadyAlertEnd(false);
+    }
+
+    if (clicked > 0) {
+        const duration = Math.ceil((time - clicked) / 1000);
+        const remains = Math.max(0, timeToPlay - duration);
+
+        if (!alreadyAlert && remains < 20) {
+            Vibration.vibrate();
+            setAlreadyAlert(true);
+        }
+
+        if (remains == 0 && !alreadyAlertEnd) {
+            Vibration.vibrate();
+            setAlreadyAlertEnd(true);
+        }
+
+        return (
+            <View style={{backgroundColor: 'green'}}>
+                <Text style={styles.counter}>{remains}</Text>
+                <Button title="Suivant" onPress={next}/>
+                <Button title="Reset" onPress={restart}/>
+            </View>
+        );
     }
 
     return (
         <View style={{backgroundColor: 'green'}}>
-            <Text style={styles.counter}>{time}</Text>
             <Button title="Casse" onPress={start}/>
-            <Button title="Vibrate once" onPress={() => {
-                Vibration.vibrate();
-                setTime(45)
-            }}/>
         </View>
     );
 };
