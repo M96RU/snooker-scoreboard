cd5b0caee78bfc2eae4d96f3ae1ffe1d1c6afb7f
M96RU <manuelrusso@laposte.net>
2024-08-26 22:41:29 +0200

    Timer simple version

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Scorer from "./pages/Scorer";
+
+const App = () => {
+    return (
+        <View>
+            <Scorer></Scorer>
+        </View>
+    );
+};
+export default App;
--- components/Timer/Timer.tsx
+++ components/Timer/Timer.tsx
@@ -1,13 +1,118 @@
 import React from 'react';
-import {Button, Text, Vibration, View,} from 'react-native';
+import {Button, StyleSheet, Text, Vibration, View} from "react-native";
 
-function Timer(): React.JSX.Element {
+export interface TimerProps {
+    timeToPlayAfterBreak: number,
+    timeToAddAfterBreak: number,
+    timeToPlayDuringGame: number,
+    timeToAddDuringGame: number,
+    alertUnderSeconds: number
+}
 
+const Timer = (props: TimerProps) => {
+
+    const ONE_SECOND_IN_MS = 1000;
+
+    const now = () => {
+        return new Date().getTime();
+    }
+
+    // current time - updated asynchronously
+    const [time, setTime] = React.useState(now());
+
+    // last click time
+    const [clicked, setClicked] = React.useState(0);
+
+    // last click time
+    const [timeToPlay, setTimeToPlay] = React.useState(props.timeToPlayAfterBreak);
+
+    const [playingAfterBreak, setPlayingAfterBreak] = React.useState(false);
+    const [alreadyExtensionA, setAlreadyExtensionA] = React.useState(false);
+    const [alreadyExtensionB, setAlreadyExtensionB] = React.useState(false);
+
+    // true after alert
+    const [alreadyVibrateWarning, setAlreadyVibrateWarning] = React.useState(false);
+    // true after alert
+    const [alreadyVibrateFault, setAlreadyVibrateFault] = React.useState(false);
+
+    React.useEffect(() => {
+        const id = setTimeout(() => {
+            setTime(now())
+        }, 300);
+        return () => {
+            clearTimeout(id);
+        };
+    });
+
+    const start = () => {
+        setClicked(now());
+        setTimeToPlay(props.timeToPlayAfterBreak);
+        setPlayingAfterBreak(true);
+        setAlreadyExtensionA(false);
+        setAlreadyExtensionB(false);
+        setAlreadyVibrateWarning(false);
+        setAlreadyVibrateFault(false);
+    }
+    const restart = () => {
+        setClicked(0);
+    }
+    const next = () => {
+        setTimeToPlay(props.timeToPlayDuringGame);
+        setClicked(now());
+        setPlayingAfterBreak(false);
+        setAlreadyVibrateWarning(false);
+        setAlreadyVibrateFault(false);
+    }
+    const extension = () => {
+        if (playingAfterBreak) {
+            setTimeToPlay(props.timeToPlayAfterBreak + props.timeToAddAfterBreak);
+        } else {
+            setTimeToPlay(props.timeToPlayDuringGame + props.timeToAddDuringGame);
+        }
+    }
+    const extensionA = () => {
+        extension();
+        setAlreadyExtensionA(true);
+    }
+    const extensionB = () => {
+        extension();
+        setAlreadyExtensionB(true);
+    }
+
+    let remains = undefined;
+
+    if (clicked > 0) {
+        const duration = Math.ceil((time - clicked) / 1000);
+        remains = Math.max(0, timeToPlay - duration);
+
+        if (!alreadyVibrateWarning && remains < props.alertUnderSeconds) {
+            Vibration.vibrate(2 * ONE_SECOND_IN_MS)
+            setAlreadyVibrateWarning(true);
+        }
+
+        if (remains === 0 && !alreadyVibrateFault) {
+            Vibration.vibrate(4 * ONE_SECOND_IN_MS)
+            setAlreadyVibrateFault(true);
+        }
+
+    }
     return (
-        <View>
-            <Button title="Vibrate once" onPress={() => Vibration.vibrate()}/>
+        <View style={{backgroundColor: 'green'}}>
+            <Text style={styles.counter}>{remains}</Text>
+            <Button disabled={clicked > 0} title="Casse" onPress={start}/>
+            <Button disabled={alreadyExtensionA || clicked === 0} title="Extension Jaunes" onPress={extensionA}/>
+            <Button disabled={alreadyExtensionB || clicked === 0} title="Extension Rouges" onPress={extensionB}/>
+            <Button disabled={clicked === 0} title="Suivant" onPress={next}/>
+            <Button disabled={clicked === 0} title="Nouvelle Partie" onPress={restart}/>
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    counter: {
+        fontSize: 100,
+        alignSelf: 'center'
+    }
+});
 
 export default Timer;
