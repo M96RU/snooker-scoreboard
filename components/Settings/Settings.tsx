1fca20fa317db22e1b8cc69a7932a89afcb89e8c
M96RU <manuelrusso@laposte.net>
2024-08-27 22:03:50 +0200

    Add FBEP and FFB settings

--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -8,6 +8,7 @@ interface SettingsProps {
 }
 
 interface SettingsState {
+    editable: boolean,
     timer: TimerProps
 }
 
@@ -16,8 +17,34 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
     constructor(props: SettingsProps) {
         super(props);
         this.state = {
+            editable: false,
             timer: props.timer
         }
+    }
+
+    edit(currentState: SettingsState) {
+        currentState.editable = true;
+        this.setState(currentState);
+    }
+
+    switchToFBEP(currentState: SettingsState) {
+        currentState.editable = false;
+        currentState.timer.timeToPlayAfterBreak = 45;
+        currentState.timer.timeToAddAfterBreak = 45;
+        currentState.timer.timeToPlayDuringGame = 45;
+        currentState.timer.timeToAddDuringGame = 45;
+        currentState.timer.alertUnderSeconds = 10;
+        this.setState(currentState);
+    }
+
+    switchToFFB(currentState: SettingsState) {
+        currentState.editable = false;
+        currentState.timer.timeToPlayAfterBreak = 90;
+        currentState.timer.timeToAddAfterBreak = 45;
+        currentState.timer.timeToPlayDuringGame = 45;
+        currentState.timer.timeToAddDuringGame = 45;
+        currentState.timer.alertUnderSeconds = 20;
+        this.setState(currentState);
     }
 
     updateTimeToPlayAfterBreak(update: string, currentState: SettingsState) {
@@ -49,8 +76,14 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
         return (
             <View>
                 <SafeAreaView>
+
+                    <Button disabled={this.state.editable} title="Modifier" onPress={() => this.edit(this.state)}/>
+                    <Button title="FBEP" onPress={() => this.switchToFBEP(this.state)}/>
+                    <Button title="FFB" onPress={() => this.switchToFFB(this.state)}/>
+
                     <Text>Temps après la casse</Text>
                     <TextInput
+                        editable={this.state.editable}
                         textAlign={'center'}
                         value={this.state.timer.timeToPlayAfterBreak.toString()}
                         onChangeText={(text: string) => this.updateTimeToPlayAfterBreak(text, this.state)}
@@ -58,6 +91,7 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
                     />
                     <Text>Extension après la casse</Text>
                     <TextInput
+                        editable={this.state.editable}
                         textAlign={'center'}
                         value={this.state.timer.timeToAddAfterBreak.toString()}
                         onChangeText={(text: string) => this.updateTimeToAddAfterBreak(text, this.state)}
@@ -65,6 +99,7 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
                     />
                     <Text>Temps de jeu</Text>
                     <TextInput
+                        editable={this.state.editable}
                         textAlign={'center'}
                         value={this.state.timer.timeToPlayDuringGame.toString()}
                         onChangeText={(text: string) => this.updateTimeToPlayDuringGame(text, this.state)}
@@ -72,6 +107,7 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
                     />
                     <Text>Extension en cours de partie</Text>
                     <TextInput
+                        editable={this.state.editable}
                         textAlign={'center'}
                         value={this.state.timer.timeToAddDuringGame.toString()}
                         onChangeText={(text: string) => this.updateTimeToAddDuringGame(text, this.state)}
@@ -79,6 +115,7 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
                     />
                     <Text>Alerte</Text>
                     <TextInput
+                        editable={this.state.editable}
                         textAlign={'center'}
                         value={this.state.timer.alertUnderSeconds.toString()}
                         onChangeText={(text: string) => this.updateAlertUnderSeconds(text, this.state)}
