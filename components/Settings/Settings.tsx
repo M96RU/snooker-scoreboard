f3f468561482e4e2e811bcd4dc72c1827d15b5a1
M96RU <manuelrusso@laposte.net>
2024-09-02 22:44:34 +0200

    Add style to Settings

--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import {Pressable, StyleSheet, Text, TextInput, View,} from 'react-native';
+import {StyleSheet, View,} from 'react-native';
 import {TimerProps} from "../Timer";
+import {Button, Card, TextInput} from "react-native-paper";
+
+enum Mode {
+    FFB, FBEP, CUSTOM
+}
 
 interface SettingsProps {
     timer: TimerProps,
@@ -9,8 +14,8 @@ interface SettingsProps {
 }
 
 interface SettingsState {
-    editable: boolean,
-    timer: TimerProps
+    timer: TimerProps,
+    mode: Mode
 }
 
 class Settings extends React.Component<SettingsProps, SettingsState> {
@@ -18,18 +23,18 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
     constructor(props: SettingsProps) {
         super(props);
         this.state = {
-            editable: false,
-            timer: props.timer
+            timer: props.timer,
+            mode: Mode.FFB
         }
     }
 
     edit(currentState: SettingsState) {
-        currentState.editable = true;
+        currentState.mode = Mode.CUSTOM;
         this.setState(currentState);
     }
 
     switchToFBEP(currentState: SettingsState) {
-        currentState.editable = false;
+        currentState.mode = Mode.FBEP;
         currentState.timer.timeToPlayAfterBreak = 45;
         currentState.timer.timeToAddAfterBreak = 45;
         currentState.timer.timeToPlayDuringGame = 45;
@@ -39,7 +44,7 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
     }
 
     switchToFFB(currentState: SettingsState) {
-        currentState.editable = false;
+        currentState.mode = Mode.FFB;
         currentState.timer.timeToPlayAfterBreak = 90;
         currentState.timer.timeToAddAfterBreak = 45;
         currentState.timer.timeToPlayDuringGame = 45;
@@ -100,89 +105,70 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
 
     render() {
         return (
-            <View>
+
+            <View style={styles.container}>
+
+                <Card style={styles.item}>
+                    <Card.Title title={'Timer'}/>
+                    <Card.Content>
+                        <TextInput
+                            label={'Casse'}
+                            editable={this.state.mode === Mode.CUSTOM}
+                            value={this.state.timer.timeToPlayAfterBreak.toString()}
+                            onChangeText={(text: string) => this.updateTimeToPlayAfterBreak(text, this.state)}
+                            keyboardType={'numeric'}
+                        />
+                        <TextInput
+                            label={'Extension casse'}
+                            editable={this.state.mode === Mode.CUSTOM}
+                            value={this.state.timer.timeToAddAfterBreak.toString()}
+                            onChangeText={(text: string) => this.updateTimeToAddAfterBreak(text, this.state)}
+                            keyboardType={'numeric'}
+                        />
+                        <TextInput
+                            label={'Visite'}
+                            editable={this.state.mode === Mode.CUSTOM}
+                            value={this.state.timer.timeToPlayDuringGame.toString()}
+                            onChangeText={(text: string) => this.updateTimeToPlayDuringGame(text, this.state)}
+                            keyboardType={'numeric'}
+                        />
+                        <TextInput
+                            label={'Extension visite'}
+                            editable={this.state.mode === Mode.CUSTOM}
+                            value={this.state.timer.timeToAddDuringGame.toString()}
+                            onChangeText={(text: string) => this.updateTimeToAddDuringGame(text, this.state)}
+                            keyboardType={'numeric'}
+                        />
+                        <TextInput
+                            label={'Alerte'}
+                            editable={this.state.mode === Mode.CUSTOM}
+                            value={this.state.timer.alertUnderSeconds.toString()}
+                            onChangeText={(text: string) => this.updateAlertUnderSeconds(text, this.state)}
+                            keyboardType={'numeric'}
+                        />
+                    </Card.Content>
+                    <Card.Actions>
+                        <Button mode={'text'} disabled={this.state.mode === Mode.CUSTOM} onPress={() => this.edit(this.state)}>Modifier</Button>
+                        <Button mode={'text'} disabled={this.state.mode === Mode.FBEP} onPress={() => this.switchToFBEP(this.state)}>FBEP</Button>
+                        <Button mode={'text'} disabled={this.state.mode === Mode.FFB} onPress={() => this.switchToFFB(this.state)}>FFB</Button>
+                    </Card.Actions>
+                </Card>
 
                 <View>
-                    <Text style={styles.itemLabel}>Joueur 1</Text>
-                    <Text style={styles.itemLabel}>Joueur 2</Text>
-                </View>
-                <View>
-                    <TextInput
-                        value={this.state.timer.playerA}
-                        onChangeText={(text: string) => this.updatePlayerA(text, this.state)}
-                    />
-                    <TextInput
-                        value={this.state.timer.playerB}
-                        onChangeText={(text: string) => this.updatePlayerB(text, this.state)}
-                    />
+                    <Card style={styles.item}>
+                        <Card.Title title={'Match'}/>
+                        <Card.Content>
+                            <TextInput label='Joueur 1' value={this.state.timer.playerA} onChangeText={(text: string) => this.updatePlayerA(text, this.state)}/>
+                            <TextInput label='Joueur 2' value={this.state.timer.playerB} onChangeText={(text: string) => this.updatePlayerB(text, this.state)}/>
+                        </Card.Content>
+                    </Card>
+
+                    <View style={[styles.item, styles.buttons]}>
+                        <Button style={styles.button} mode={'contained-tonal'} onPress={() => this.props.onClose()}>Fermer</Button>
+                        <Button style={styles.button} mode={'contained'} onPress={() => this.props.onChange(this.state.timer)}>Sauvegarder</Button>
+                    </View>
                 </View>
 
-                <View>
-                    <Pressable disabled={this.state.editable} onPress={() => this.edit(this.state)}>
-                        <Text>MODIFIER</Text>
-                    </Pressable>
-                    <Pressable  onPress={() => this.switchToFBEP(this.state)}>
-                        <Text>FBEP</Text>
-                    </Pressable>
-                    <Pressable onPress={() => this.switchToFFB(this.state)}>
-                        <Text>FFB</Text>
-                    </Pressable>
-                </View>
-
-                <View style={styles.item}>
-                    <Text>Casse</Text>
-                    <TextInput
-                        editable={this.state.editable}
-                        value={this.state.timer.timeToPlayAfterBreak.toString()}
-                        onChangeText={(text: string) => this.updateTimeToPlayAfterBreak(text, this.state)}
-                        keyboardType={'numeric'}
-                    />
-                </View>
-                <View>
-                    <Text>Extension casse</Text>
-                    <TextInput
-                        editable={this.state.editable}
-                        value={this.state.timer.timeToAddAfterBreak.toString()}
-                        onChangeText={(text: string) => this.updateTimeToAddAfterBreak(text, this.state)}
-                        keyboardType={'numeric'}
-                    />
-                </View>
-                <View>
-                    <Text>Visite</Text>
-                    <TextInput
-                        editable={this.state.editable}
-                        value={this.state.timer.timeToPlayDuringGame.toString()}
-                        onChangeText={(text: string) => this.updateTimeToPlayDuringGame(text, this.state)}
-                        keyboardType={'numeric'}
-                    />
-                </View>
-                <View>
-                    <Text>Extension visite</Text>
-                    <TextInput
-                        editable={this.state.editable}
-                        value={this.state.timer.timeToAddDuringGame.toString()}
-                        onChangeText={(text: string) => this.updateTimeToAddDuringGame(text, this.state)}
-                        keyboardType={'numeric'}
-                    />
-                </View>
-                <View>
-                    <Text>Alerte</Text>
-                    <TextInput
-                        editable={this.state.editable}
-                        value={this.state.timer.alertUnderSeconds.toString()}
-                        onChangeText={(text: string) => this.updateAlertUnderSeconds(text, this.state)}
-                        keyboardType={'numeric'}
-                    />
-                </View>
-
-                <View>
-                    <Pressable onPress={() => this.props.onClose()}>
-                        <Text>FERMER</Text>
-                    </Pressable>
-                    <Pressable onPress={() => this.props.onChange(this.state.timer)}>
-                        <Text>VALIDER</Text>
-                    </Pressable>
-                </View>
             </View>
         )
     }
@@ -191,40 +177,18 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         flexDirection: 'column'
+    },
+    item: {
+        margin: 10
     },
     buttons: {
         flexDirection: 'row',
     },
     button: {
         flex: 1,
-        margin: 2,
-        padding: 4,
-        borderRadius: 5,
-        backgroundColor: 'blue'
-    },
-    buttonText: {
-        textAlign: 'center',
-        color: 'white'
-    },
-    item: {
-        padding: 3,
-        flexDirection: 'row',
-    },
-    itemLabel: {
-        flex: 2,
-        padding: 2,
-        textAlign: 'right'
-    },
-    itemInput: {
-        flex: 1,
-        padding: 2,
-        textAlign: 'center',
-        borderWidth: 1,
-        margin: 2,
-        borderRadius: 5
-    },
+        margin: 5
+    }
 });
 
 export default Settings;
