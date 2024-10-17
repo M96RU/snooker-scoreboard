73c76b68757c3d6b41a1f53ad2cdea294f12a8e3
M96RU <m96ru.info@gmail.com>
2024-10-17 22:13:22 +0200

    update menu

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,33 @@
-import {Tabs} from "expo-router";
-import {TabBarIcon} from '@/components/navigation/TabBarIcon';
-import React from 'react';
+import 'react-native-gesture-handler';
 
-export default function RootLayout() {
-    return (
-        <Tabs>
-            <Tabs.Screen name="live" options={
-                {
-                    title: 'Live',
-                    headerShown: false,
-                    tabBarIcon: ({color, focused}) => (
-                        <TabBarIcon name={focused ? 'walk' : 'walk-outline'} color={color}/>
-                    )
-                }
-            }/>
-            <Tabs.Screen name="index" options={
-                {
-                    title: 'Home',
-                    headerShown: false,
-                    tabBarIcon: ({color, focused}) => (
-                        <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color}/>
-                    )
-                }
-            }/>
-        </Tabs>
-    );
+import React from 'react';
+import {Drawer} from 'expo-router/drawer';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import CustomMenu from '@/components/CustomMenu';
+import {TabBarIcon} from '@/components/navigation/TabBarIcon';
+
+export default function DrawerLayout() {
+    return <GestureHandlerRootView style={{flex: 1}}>
+        <Drawer drawerContent={CustomMenu} screenOptions={{
+            drawerHideStatusBarOnOpen: true,
+            drawerActiveBackgroundColor: '#5363df',
+            drawerActiveTintColor: 'white',
+            drawerLabelStyle: {marginLeft: -20}
+        }}>
+            <Drawer.Screen name={'live'} options={{
+                drawerLabel: 'Live',
+                headerTitle: 'Live',
+                drawerIcon: ({color, focused}) => (
+                    <TabBarIcon name={focused ? 'walk' : 'walk-outline'} color={color}/>
+                )
+            }}/>
+            <Drawer.Screen name={'index'} options={{
+                drawerLabel: 'Index',
+                headerTitle: 'Index',
+                drawerIcon: ({color, focused}) => (
+                    <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color}/>
+                )
+            }}/>
+        </Drawer>
+    </GestureHandlerRootView>
 }
